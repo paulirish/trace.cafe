16cd842aa25ed82d5ca071aa4a3cabba2a3bbc9e
Paul Irish <commits@paul.irish>
2023-07-18 11:23:02 -0700

    adding perfetto ui - prototype

--- src/app.js
+++ src/app.js
@@ -8,10 +8,10 @@ import {hijackConsole} from './log';
 // TODO: find a way to update this as it's currently frozen in time .. or make sure it matches the trace version?
 //    Current workflow: grab the Revision from chrome:version
 //    These hashes match up with the "Incrementing VERSION" commits: https://chromium.googlesource.com/chromium/src/+log/111.0.5544.2..111.0.5544.3?pretty=fuller&n=10000
-const devtoolsGitHash = '030cc140435b0152645522b9864b75cac6c0a854'; // 112.0.5615.20 should be the 112 beta branch.  
-                                                                    // Has interrupted flamechart fix and timelineloader .traceEvents fix. 
-                                                                    // No tracemodel worker. No trace engine at all.
-                                                                    // Has 'fixing samples' message not emitted to users.
+const devtoolsGitHash = '030cc140435b0152645522b9864b75cac6c0a854'; // 112.0.5615.20 should be the 112 beta branch.
+// Has interrupted flamechart fix and timelineloader .traceEvents fix.
+// No tracemodel worker. No trace engine at all.
+// Has 'fixing samples' message not emitted to users.
 
 // Ideally we'd use `devtools://devtools/bundled/js_app.html...` …
 //     but the browser has extra protection on devtools:// URLS..
@@ -29,19 +29,19 @@ const devtoolsBaseUrl = `https://chrome-devtools-frontend.appspot.com/serve_rev/
  * @param {ParentNode=} context
  * @return {ParseSelector<T>}
  */
-globalThis.$ = function(query, context) {
+globalThis.$ = function (query, context) {
   const result = (context || document).querySelector(query);
   if (result === null) {
     throw new Error(`query ${query} not found`);
   }
   return /** @type {ParseSelector<T>} */ (result);
-}
+};
 
 /**
  * Show devtools now that we have a trace asset URL
- * @param {string | undefined} assetUrl 
- * @param {FullMetadata} fileData 
- * @returns 
+ * @param {string | undefined} assetUrl
+ * @param {FullMetadata} fileData
+ * @returns
  */
 async function displayTrace(assetUrl, fileData) {
   if (!assetUrl) {
@@ -50,7 +50,7 @@ async function displayTrace(assetUrl, fileData) {
   }
 
   document.body.className = 'state--viewing';
-  
+
   // Set human-friendly title
   const filename = (fileData?.metadata?.oName || fileData.name).replace('.json', '');
   const date = new Date(fileData.timeCreated);
@@ -58,27 +58,66 @@ async function displayTrace(assetUrl, fileData) {
   const dateStr = fmter.format(date);
   document.title = `trace.cafe — ${filename} — ${dateStr}`;
 
-  setTimeout(_ => {$('details').open = false;}, 1_000);
+  setTimeout(_ => {
+    $('details').open = false;
+  }, 1_000);
 
   /**
-  * For the object data URL (in getAssetUrl) we just encode the traces/traceid path once. but here we do it TWICE. Why????!
-  * Because DevTools incorrectly double decodes the path.
-  *  - First decode in `Runtime.Runtime.queryParam()` with the searchParams.get call
-  *  - Second decode in TimelinePanel's `handleQueryParam()` for loadTimelineFromURL it happens again.
-  * TODO: fix that bug in devtools.
-  */
+   * For the object data URL (in getAssetUrl) we just encode the traces/traceid path once. but here we do it TWICE. Why????!
+   * Because DevTools incorrectly double decodes the path.
+   *  - First decode in `Runtime.Runtime.queryParam()` with the searchParams.get call
+   *  - Second decode in TimelinePanel's `handleQueryParam()` for loadTimelineFromURL it happens again.
+   * TODO: fix that bug in devtools.
+   */
   const encodedAssetUrl = encodeURIComponent(assetUrl);
   const hostedDtViewingTraceUrl = new URL(devtoolsBaseUrl);
   hostedDtViewingTraceUrl.searchParams.set('loadTimelineFromURL', encodedAssetUrl);
 
   console.log('Trace opening in DevTools…', filename);
-  const iframe = $('iframe#ifr');
+  const iframe = $('iframe#ifr-dt');
   iframe.onload = _ => {
-    // Technically devtools iframe just loaded (didnt 404). We assume the trace loaded succfessully too. 
+    // Technically devtools iframe just loaded (didnt 404). We assume the trace loaded succfessully too.
     // Can't really extract errors from that iframe.....
     console.log('Trace loaded.', filename, 'Uploaded:', dateStr);
-  }
+    showTraceInPerfetto(iframePftto, assetUrl, filename);
+  };
   iframe.src = hostedDtViewingTraceUrl.href;
+
+  // warm up perfetto iframe
+  const iframePftto = $('iframe#ifr-pftto');
+  iframePftto.src = 'https://ui.perfetto.dev/';
+  globalThis.iframePftto = iframePftto;
+  // TODO: move this, so its only called once
+}
+
+// https://perfetto.dev/docs/visualization/deep-linking-to-perfetto-ui
+async function showTraceInPerfetto(iframePftto, assetUrl, filename) {
+  const ORIGIN = 'https://ui.perfetto.dev';
+  const timer = setInterval(() => iframePftto.contentWindow.postMessage('PING', ORIGIN), 50);
+
+  const perfettoOnMessage = async evt => {
+    if (evt.data !== 'PONG') return;
+
+    // We got a PONG, the UI is ready.
+    window.clearInterval(timer);
+    window.removeEventListener('message', perfettoOnMessage);
+
+
+    const buffer = await fetch(assetUrl).then(r => r.arrayBuffer());
+
+    // send trace
+    iframePftto.contentWindow.postMessage(
+      {
+        perfetto: {
+          buffer: buffer,
+          title: filename,
+        },
+      },
+      ORIGIN
+    );
+  };
+
+  window.addEventListener('message', perfettoOnMessage);
 }
 
 async function readParams() {
@@ -98,7 +137,7 @@ async function readParams() {
 function setupLanding() {
   // // Preload iframe
   // TODO: use Navigation API to avoid the preload from adding a history entry.
-  // const iframe = $('iframe#ifr');
+  // const iframe = $('iframe#ifr-dt');
   // iframe.src = `${devtoolsBaseUrl}?loadTimelineFromURL=`;
 
   // Update example trace URL
@@ -113,9 +152,9 @@ function setupLanding() {
     location.href = '/';
   });
 
-  addEventListener('unhandledrejection', (event) => {
-    console.error('Unhandled rejection: ', event?.reason?.message)
-   });
+  addEventListener('unhandledrejection', event => {
+    console.error('Unhandled rejection: ', event?.reason?.message);
+  });
 }
 
 function setupFileInput() {

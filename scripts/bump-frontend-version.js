ad06a30d6d1a56ef135b1ab3b39999dbf72a4b7f
Paul Irish <commits@paul.irish>
2024-11-08 07:36:53 -0800

    bump script's test url should load something real

--- scripts/bump-frontend-version.js
+++ scripts/bump-frontend-version.js
@@ -26,4 +26,7 @@ fs.writeFileSync('./src/app.js', text);
 
 console.log('app.js updated.')
 
-console.log(`open this to warm the caches:\n    https://chrome-devtools-frontend.appspot.com/serve_rev/@${hash}/rehydrated_devtools_app.html?loadTimelineFromURL=http://localhost:40404/404`)
+
+const encodedDemoTraceUrl = `https%253A%252F%252Ffirebasestorage.googleapis.com%252Fv0%252Fb%252Ftum-permatraces2%252Fo%252Fpermatraces%25252F7qvReGZ6RU%253Falt%253Dmedia%2526token%253D934388e9-421b-471b-8f26-eebbf97d75e0`;
+
+console.log(`open this to warm the caches:\n    https://chrome-devtools-frontend.appspot.com/serve_rev/@${hash}/rehydrated_devtools_app.html?loadTimelineFromURL=${encodedDemoTraceUrl}`)

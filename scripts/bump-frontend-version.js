c1cfaadc30c770a7f0871c715e944e40992c7d6e
Paul Irish <commits@paul.irish>
2024-11-12 14:02:10 -0800

    add pw script to populate the appspot cache. needs some cron :)

--- scripts/bump-frontend-version.js
+++ scripts/bump-frontend-version.js
@@ -1,35 +1,49 @@
 import * as fs from 'node:fs';
+import { attemptLoad } from './load-appspot-page-for-cachewarming.js';
 
 // see go/hyvwn
 
-console.log('Fetching latest release data.')
-const data = await fetch("https://chromiumdash.appspot.com/fetch_releases?platform=Linux&channel=Canary", {
-  "headers": {
-    "accept": "application/json",
-    "accept-language": "en-US,en;q=0.9",
-    "cache-control": "no-cache",
-    "pragma": "no-cache"
-  },
-  "method": "GET"
-}).then(r => r.json());
+export async function main() {
+  console.log('Fetching latest release data.');
+  const data = await fetch('https://chromiumdash.appspot.com/fetch_releases?platform=Linux&channel=Canary', {
+    headers: {
+      accept: 'application/json',
+      'accept-language': 'en-US,en;q=0.9',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+    },
+    method: 'GET',
+  }).then(r => r.json());
 
-const release = data[0];
-const hash = release.hashes.chromium;
-const devtoolsHash = release.hashes.devtools;
-const version = release.version;
+  const release = data[0];
+  const hash = release.hashes.chromium;
+  const devtoolsHash = release.hashes.devtools;
+  const version = release.version;
 
-console.log('Found', {version, hash, devtoolsHash});
-const declaration = `const chromiumHashVer = ['${hash}', '${version}'];`
+  console.log('Found', {version, hash, devtoolsHash});
+  const declaration = `const chromiumHashVer = ['${hash}', '${version}'];`;
 
-let text = fs.readFileSync('./src/app.js', 'utf-8');
-text = text.replace(/const chromiumHashVer.*/, declaration);
-fs.writeFileSync('./src/app.js', text);
+  let text = fs.readFileSync('./src/app.js', 'utf-8');
+  text = text.replace(/const chromiumHashVer.*/, declaration);
+  fs.writeFileSync('./src/app.js', text);
 
-console.log('app.js updated.\n');
+  console.log('app.js updated.\n');
 
+  console.log('\nWhats new in this version: ➡️', `https://chromium.googlesource.com/devtools/devtools-frontend/+log/${devtoolsHash}`);
+
+  // console.log(`\nWarm the caches ➡️`, appspotUrl(hash), '\n');
+  attemptLoad();
+}
 
 const encodedDemoTraceUrl = `https%253A%252F%252Ffirebasestorage.googleapis.com%252Fv0%252Fb%252Ftum-permatraces2%252Fo%252Fpermatraces%25252F7qvReGZ6RU%253Falt%253Dmedia%2526token%253D934388e9-421b-471b-8f26-eebbf97d75e0`;
 
-console.log('\nWhat\s new in this version: ➡️', `https://chromium.googlesource.com/devtools/devtools-frontend/+log/${devtoolsHash}`);
+export const appspotUrl = hash =>
+  `https://chrome-devtools-frontend.appspot.com/serve_rev/@${hash}/rehydrated_devtools_app.html?loadTimelineFromURL=${encodedDemoTraceUrl}`;
 
-console.log(`\nWarm the caches ➡️`, `https://chrome-devtools-frontend.appspot.com/serve_rev/@${hash}/rehydrated_devtools_app.html?loadTimelineFromURL=${encodedDemoTraceUrl}`, '\n');
+
+
+// CLI direct invocation?
+if (import.meta.url.endsWith(process?.argv[1])) {
+  main();
+}
+
---
+++ scripts/load-appspot-page-for-cachewarming.js
@@ -0,0 +1,65 @@
+import {chromium} from 'playwright';
+import fs from 'node:fs';
+import path from 'node:path';
+import {appspotUrl} from './bump-frontend-version.js';
+
+export async function attemptLoad () {
+  // Get chromiumHashVer from command line argument or app.js
+  let chromiumHashVer = process.argv[2];
+  if (!chromiumHashVer) {
+    try {
+      const filename = path.resolve(process.cwd(), './src/app.js');
+      const appJsContent = fs.readFileSync(filename, 'utf8');
+      const match = appJsContent.match(/const chromiumHashVer = \[.(\w+)'/);
+      if (match && match[1]) {
+        chromiumHashVer = match[1];
+      } else {
+        console.error("Couldn't find chromiumHashVer in app.js");
+        process.exit(1);
+      }
+    } catch (err) {
+      console.error('Failed to read app.js:', err);
+      process.exit(1);
+    }
+  }
+
+  const browser = await chromium.launch({
+    // headless: false,
+  });
+  const page = await browser.newPage();
+  const url = appspotUrl(chromiumHashVer);
+  const attempts = 5;
+  console.log('\nAttempting to load', url);
+  page.on('request', () => process.stdout.write('•'));
+  
+  for (let i = 0; i < attempts; i++) {
+    console.log(`    [${i + 1} / ${attempts}]`);
+    try {
+      const response = await page.goto(url);
+
+      if (response.status() === 404) {
+        console.log('\nPage not found (404). Exiting.');
+        break;
+      }
+
+      await page.waitForLoadState('networkidle', {timeout: 120_000});
+      console.log('\n😊 Network is quiet! Ensuring text is there');
+      await page.waitForSelector('text=Bottom-up', {timeout: 5_000});
+      console.log('✅ Found Bottom-up!');
+      break;
+
+    } catch (error) {
+      console.log('Network did not become quiet within 120 seconds.');
+      console.log(i < 4 ? 'Retrying...' : 'Giving up.');
+    }
+  }
+
+  await browser.close();
+}
+
+
+// CLI direct invocation?
+if (import.meta.url.endsWith(process?.argv[1])) {
+  attemptLoad();
+}
+

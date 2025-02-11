cf352df37cf09fc9de8b6c7041c8eaf4f056427a
Paul Irish <commits@paul.irish>
2025-02-11 10:01:24 -0800

    Revert "bump to tot"
    This reverts commit 55cdb0270129334fb78bd1a59109427b9c44a3a4.

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import {upload} from './storage';
 // TODO: find a way to update this as it's currently frozen in time .. or make sure it matches the trace version?
 //    Current workflow: grab the Revision from chrome:version
 //    These hashes match up with the "Updating trunk VERSION" commits: https://chromium.googlesource.com/chromium/src/+log/main/chrome/VERSION
-const chromiumHashVer = ['07bde3181fa9396ec0ccab4665463c0400a20b60', '135.0.7011.0'];
+const chromiumHashVer = ['0155b8edb17ab54ddbc7f4b79a160632275fbbd5', '133.0.6943.0'];
 
 // Ideally we'd use `devtools://devtools/bundled/js_app.html...` …
 //     but the browser has extra protection on devtools:// URLS..

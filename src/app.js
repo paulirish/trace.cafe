21ad8aee69d24e53817671929c0838a362bf30ad
Paul Irish <commits@paul.irish>
2025-11-12 14:45:12 +0100

    bump

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import {upload} from './storage';
 // TODO: find a way to update this as it's currently frozen in time .. or make sure it matches the trace version?
 //    Current workflow: grab the Revision from chrome:version
 //    These hashes match up with the "Updating trunk VERSION" commits: https://chromium.googlesource.com/chromium/src/+log/main/chrome/VERSION
-const chromiumHashVer = ['f2f8504343c8da40486cd22d97202589972ec515', '143.0.7497.0'];
+const chromiumHashVer = ['513a8a918598025345c4188582f14155eca5fe87', '144.0.7524.0'];
 
 // Ideally we'd use `devtools://devtools/bundled/js_app.html...` …
 //     but the browser has extra protection on devtools:// URLS..
